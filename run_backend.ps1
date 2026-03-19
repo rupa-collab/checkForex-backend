71c22fba66131067201859f10fa1cbe84bcb8ebb
RupaAssad <rupa.assad21@gmail.com>
2026-03-19 21:59:59 +0530

    Add VS Code config and run script

---
+++ run_backend.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [int]$Port = 8000
+)
+
+if (!(Test-Path .venv)) {
+    python -m venv .venv
+}
+
+.\.venv\Scripts\pip install -r requirements.txt
+.\.venv\Scripts\uvicorn app.main:app --reload --host 0.0.0.0 --port $Port
